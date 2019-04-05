097f08818d8d20542aba010d3bce3e049d07f22a
n-okonek <41804767+n-okonek@users.noreply.github.com>
2019-04-05 17:06:59 -0500

    Final DOM hack change

--- DOM/DOMHack.js
+++ DOM/DOMHack.js
@@ -9,7 +9,7 @@ var headers = document.getElementsByTagName('h2'),
     productList = [];
 
 
-// add buttons to page
+// add extraction buttons to page
 headers[0].after(button0);
 headers[1].after(button1);
 
@@ -20,16 +20,19 @@ for (var i = 0; i < buttons.length; i++){
 buttons[0].setAttribute("onclick", "extractAccounts()");
 buttons[1].setAttribute("onclick", "extractProducts()");
 
-// add sort 
-accounTable.getElementsByTagName('tr')[0].children[0].innerHTML = "Name <button onclick='sortAccounts(true, false)'>Sort</button>";
-accounTable.getElementsByTagName('tr')[0].children[1].innerHTML = "Email <button onclick='sortAccounts(false, true)'>Sort</button>";
-prodTable[0].getElementsByTagName('tr')[0].children[1].innerHTML = "Name <button onclick='sortProducts(true, false)'>Sort</button>";
-prodTable[0].getElementsByTagName('tr')[0].children[2].innerHTML = "Price <button onclick='sortProducts(false, true)'>Sort</button>";
+// add sort buttons
+accounTable.getElementsByTagName('tr')[0].children[0].innerHTML = "Name <button onclick='sortAccounts(true, false)'>Sort Asc</button>";
+accounTable.getElementsByTagName('tr')[0].children[1].innerHTML = "Email <button onclick='sortAccounts(false, true)'>Sort Asc</button>";
+prodTable[0].getElementsByTagName('tr')[0].children[1].innerHTML = "Name <button onclick='sortProducts(true, false)'>Sort Asc</button>";
+prodTable[0].getElementsByTagName('tr')[0].children[2].innerHTML = "Price <button onclick='sortProducts(false, true)'>Sort Asc</button>";
 
 
 ////////////////////////////////
 /// Start Extract Functions ////
 ////////////////////////////////
+
+
+////////Extract at load/////////
 
 for (var i = 1 ; i < accounTable.getElementsByTagName('tr').length; i++){
   var fullname, fName, lName, eMail;
@@ -57,9 +60,11 @@ for (var i = 1 ; i < prodTable[0].getElementsByTagName('tr').length; i++){
   pName = prodTable[0].getElementsByTagName('tr')[i].children[1].textContent;
   price = prodTable[0].getElementsByTagName('tr')[i].children[2].textContent.replace('$', '');
 
-  productList.push( {"imgSource":img, "prodName":pName, "price":price})
+  productList.push( {"imgSource":img, "prodName":pName, "price":price});
 }
 
+
+////Display accounts onclick////
 function extractAccounts(){
   for (var i = 0; i < accountList.length; i++){
     console.log(accountList[i].FirstName + ", " + accountList[i].LastName + ", " + accountList[i].Email);
@@ -68,6 +73,7 @@ function extractAccounts(){
 
 ////////////////////////////////
 
+////Display products onclick////
 function extractProducts(){
   for ( var i = 0; i < productList.length; i++){
   console.log(productList[i].imgSource + ", " + productList[i].prodName + ", " + productList[i].price);
@@ -77,72 +83,152 @@ function extractProducts(){
 ////////////////////////////////
 ///// Start Sort Functions /////
 ////////////////////////////////
+var nAsc = false, eAsc = false, prdAsc = false, prcAsc = false;
 
 function sortAccounts(n, ea){
+  ///Sort accounts by last name///
   if(n){
-    accountList.sort(function(a,b){
-      var nameA = a.FullName.toUpperCase();
-      var nameB = b.FullName.toUpperCase();
-
-      if (nameA < nameB ){
-        return -1;
-      }else if( nameA > nameB ){
-        return 1;
-      }else{return 0;}
-    });
+    if(!nAsc){
+      accountList.sort(function(a,b){
+        var nameA = a.LastName.toUpperCase();
+        var nameB = b.LastName.toUpperCase();
+        
+        if (nameA < nameB ){
+          return -1;
+        }else if( nameA > nameB ){
+          return 1;
+        }else{return 0;}
+      });
+    
+      for (var i = 1, j = 0 ; i < accounTable.getElementsByTagName('tr').length; i++, j++){
+        accounTable.getElementsByTagName('tr')[i].children[0].textContent = accountList[j].FullName;
+        accounTable.getElementsByTagName('tr')[i].children[1].textContent = accountList[j].Email;
+      }
+      nAsc = true;
+      accounTable.getElementsByTagName('tr')[0].children[0].children[0].textContent = "Sort Desc";
+   }else if(nAsc){
+      accountList.sort(function(a,b){
+        var nameA = a.LastName.toUpperCase();
+        var nameB = b.LastName.toUpperCase();
+        if (nameA > nameB ){
+          return -1;
+        }else if( nameA < nameB ){
+          return 1;
+        }else{return 0;}
+      });
   
-    for (var i = 1, j = 0 ; i < accounTable.getElementsByTagName('tr').length; i++, j++){
-      accounTable.getElementsByTagName('tr')[i].children[0].textContent = accountList[j].FullName;
-      accounTable.getElementsByTagName('tr')[i].children[1].textContent = accountList[j].Email;
+      for (var i = 1, j = 0 ; i < accounTable.getElementsByTagName('tr').length; i++, j++){
+        accounTable.getElementsByTagName('tr')[i].children[0].textContent = accountList[j].FullName;
+        accounTable.getElementsByTagName('tr')[i].children[1].textContent = accountList[j].Email;
+      }
+      nAsc = false;
+      accounTable.getElementsByTagName('tr')[0].children[0].children[0].textContent = "Sort Asc";
     }
   }
 
+  /////Sort accounts by email/////
   if(ea){
-    accountList.sort(function(a,b){
-      var nameA = a.Email.toUpperCase();
-      var nameB = b.Email.toUpperCase();
+    if(!eAsc){
+      accountList.sort(function(a,b){
+        var nameA = a.Email.toUpperCase();
+        var nameB = b.Email.toUpperCase();
 
-      if (nameA < nameB ){
-        return -1;
-      }else if( nameA > nameB ){
-        return 1;
-      }else{return 0;}
-    });
-  
-    for (var i = 1, j = 0 ; i < accounTable.getElementsByTagName('tr').length; i++, j++){
-      accounTable.getElementsByTagName('tr')[i].children[0].textContent = accountList[j].FullName;
-      accounTable.getElementsByTagName('tr')[i].children[1].textContent = accountList[j].Email;
+        if (nameA < nameB ){
+          return -1;
+        }else if( nameA > nameB ){
+          return 1;
+        }else{return 0;}
+      });
+    
+      for (var i = 1, j = 0 ; i < accounTable.getElementsByTagName('tr').length; i++, j++){
+        accounTable.getElementsByTagName('tr')[i].children[0].textContent = accountList[j].FullName;
+        accounTable.getElementsByTagName('tr')[i].children[1].textContent = accountList[j].Email;
+      }
+      eAsc = true;
+      accounTable.getElementsByTagName('tr')[0].children[1].children[0].textContent = "Sort Desc";
+    }else if(eAsc){
+      accountList.sort(function(a,b){
+        var nameA = a.Email.toUpperCase();
+        var nameB = b.Email.toUpperCase();
+
+        if (nameA > nameB ){
+          return -1;
+        }else if( nameA < nameB ){
+          return 1;
+        }else{return 0;}
+      });
+    
+      for (var i = 1, j = 0 ; i < accounTable.getElementsByTagName('tr').length; i++, j++){
+        accounTable.getElementsByTagName('tr')[i].children[0].textContent = accountList[j].FullName;
+        accounTable.getElementsByTagName('tr')[i].children[1].textContent = accountList[j].Email;
+      }
+      eAsc = false;
+      accounTable.getElementsByTagName('tr')[0].children[1].children[0].textContent = "Sort Asc";
     }
   }
 }
 
 function sortProducts(prd, prc){
+  /////Sort products by name//////
   if(prd){
-    productList.sort(function(a,b){
-      var nameA = a.prodName.toUpperCase();
-      var nameB = b.prodName.toUpperCase();
+    if(!prdAsc){
+      productList.sort(function(a,b){
+        var nameA = a.prodName.toUpperCase();
+        var nameB = b.prodName.toUpperCase();
 
-      if (nameA < nameB ){
-        return -1;
-      }else if( nameA > nameB ){
-        return 1;
-      }else{return 0;}
-    });
-  
-    for (var i = 1, j = 0 ; i < prodTable[0].getElementsByTagName('tr').length; i++, j++){
-      //prodTable[0].getElementsByTagName('tr')[i].children[0].childNodes = 
-      prodTable[0].getElementsByTagName('tr')[i].children[1].textContent = productList[j].prodName;
-      prodTable[0].getElementsByTagName('tr')[i].children[2].textContent = "$" + productList[j].price;
+        if (nameA < nameB ){
+          return -1;
+        }else if( nameA > nameB ){
+          return 1;
+        }else{return 0;}
+      });
+    
+      for (var i = 1, j = 0 ; i < prodTable[0].getElementsByTagName('tr').length; i++, j++){
+        prodTable[0].getElementsByTagName('tr')[i].children[1].textContent = productList[j].prodName;
+        prodTable[0].getElementsByTagName('tr')[i].children[2].textContent = "$" + productList[j].price;
+      }
+      prdAsc = true;
+      prodTable[0].getElementsByTagName('tr')[0].children[1].children[0].textContent = "Sort Desc";
+    }else if(prdAsc){
+      productList.sort(function(a,b){
+        var nameA = a.prodName.toUpperCase();
+        var nameB = b.prodName.toUpperCase();
+
+        if (nameA > nameB ){
+          return -1;
+        }else if( nameA < nameB ){
+          return 1;
+        }else{return 0;}
+      });
+    
+      for (var i = 1, j = 0 ; i < prodTable[0].getElementsByTagName('tr').length; i++, j++){
+        prodTable[0].getElementsByTagName('tr')[i].children[1].textContent = productList[j].prodName;
+        prodTable[0].getElementsByTagName('tr')[i].children[2].textContent = "$" + productList[j].price;
+      }
+      prdAsc = false;
+      prodTable[0].getElementsByTagName('tr')[0].children[1].children[0].textContent = "Sort Asc";
     }
   }
-
+  /////Sort products by price/////
   if(prc){
-    productList.sort((a,b) => a.price - b.price);
-      
-    for (var i = 1, j = 0 ; i < prodTable[0].getElementsByTagName('tr').length; i++, j++){
-      //prodTable[0].getElementsByTagName('tr')[i].children[0].childNodes = 
-      prodTable[0].getElementsByTagName('tr')[i].children[1].textContent = productList[j].prodName;
-      prodTable[0].getElementsByTagName('tr')[i].children[2].textContent = "$" + productList[j].price;
+    if (!prcAsc){
+      productList.sort((a,b) => a.price - b.price);
+        
+      for (var i = 1, j = 0 ; i < prodTable[0].getElementsByTagName('tr').length; i++, j++){
+        prodTable[0].getElementsByTagName('tr')[i].children[1].textContent = productList[j].prodName;
+        prodTable[0].getElementsByTagName('tr')[i].children[2].textContent = "$" + productList[j].price;
+      }
+      prcAsc = true;
+      prodTable[0].getElementsByTagName('tr')[0].children[2].children[0].textContent = "Sort Desc";
+    }else if (prcAsc){
+      productList.sort((a,b) => b.price - a.price);
+        
+      for (var i = 1, j = 0 ; i < prodTable[0].getElementsByTagName('tr').length; i++, j++){
+        prodTable[0].getElementsByTagName('tr')[i].children[1].textContent = productList[j].prodName;
+        prodTable[0].getElementsByTagName('tr')[i].children[2].textContent = "$" + productList[j].price;
+      }
+      prcAsc = false;
+      prodTable[0].getElementsByTagName('tr')[0].children[2].children[0].textContent = "Sort Asc";
     }
   }
 }
